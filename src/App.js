bb31d1aba000febcc777de58148cd5755472e6d0
HafizMuhammad0101 <AsgharShahzad720@gmail.com>
2022-11-30 23:19:40 +0500

    Basic App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Application</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
