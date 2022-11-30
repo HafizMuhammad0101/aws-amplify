45887d9cba9a25d34119ff3c30766685d5a85b90
HafizMuhammad0101 <AsgharShahzad720@gmail.com>
2022-11-30 23:31:11 +0500

    some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <h1>I am an Imaginary Navbar!!!</h1>
+      <h1>I am Hafiz Muhammad Bin Asghar.</h1>
+      <h1>I am a Programmer By Profession!!</h1>
+      <h1>I Love JavaScript!</h1>
     </div>
   );
 }
