b5139a97a28e1b4e4790bf12d018666f747e5cef
HafizMuhammad0101 <AsgharShahzad720@gmail.com>
2022-12-12 22:28:10 +0500

    SomeChanges

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ export default function Navbar() {
       <h1>I am Hafiz Muhammad Bin Asghar.</h1>
       <h1>I am a Programmer By Profession!!</h1>
       <h1>I Love JavaScript!</h1>
+      <h1>Deployed With Netlify Mughees Bhai!! and its CI/CD</h1>
     </div>
   );
 }
